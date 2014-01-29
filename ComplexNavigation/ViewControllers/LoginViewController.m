d64ea706816b9820c14ab6e089696f6725e4138b
VASANTH K <info@effindi.com>
2014-01-29 18:39:16 +0530

    commit added

--- ComplexNavigation/ViewControllers/LoginViewController.m
+++ ComplexNavigation/ViewControllers/LoginViewController.m
@@ -36,13 +36,21 @@
     
     [prefs setObject:@"1" forKey:@"SHOW_ANOTHER_SCREEN"];
     [prefs synchronize];
+    
+    //Add Another screen into the navigation stack
     NSMutableArray *viewController=[[NSMutableArray alloc]initWithArray:self.navigationController.viewControllers];
     
     [viewController insertObject:vc atIndex:viewController.count-1];
     
     [self.navigationController setViewControllers:viewController];
+    
+    //When pop, It will go the another screen instead of Main
     [self.navigationController popViewControllerAnimated:YES];
-//    [self.navigationController pushViewController:vc animated:YES];
+    
+    
+    //If u want to do this stuff in one place mean u can use delegate in MainController and call it form here to do this stuff.
+
+    //    [self.navigationController pushViewController:vc animated:YES];
     
 }
 
